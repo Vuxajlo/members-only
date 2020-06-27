f02212f242458ab521ca9b81432b2c696999d875
Vuxajlo <vukmijatovic@gmail.com>
2020-06-27 12:28:09 +0200

    basic functionality completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_sign_up_params, if: :devise_controller?
+
+    protected
+
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+     end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :members
+  devise_for :members, controllers: {
+    sessions: 'members/sessions'
+  }
+
   resources :posts, only: [:new, :create, :index]
   root "posts#index"
 end
